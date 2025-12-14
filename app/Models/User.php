3ddcc2af3055eed15131a21880b9ba6d793dab86
Hurşit Emre Duru <hemreduru@gmail.com>
2025-12-14 13:33:50 +0200

    Allow all users to access Filament panel in production

--- app/Models/User.php
+++ app/Models/User.php
@@ -7,10 +7,15 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements \Filament\Models\Contracts\FilamentUser
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
+
+    public function canAccessPanel(\Filament\Panel $panel): bool
+    {
+        return true;
+    }
 
     /**
      * The attributes that are mass assignable.
